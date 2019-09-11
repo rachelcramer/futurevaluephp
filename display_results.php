70fe53859374504e20a3a00768e7354bf7cff5ff
rachelcramer <34218608+rachelcramer@users.noreply.github.com>
2019-09-11 10:38:44 -0500

    Update display_results.php

--- display_results.php
+++ display_results.php
@@ -1,12 +1,91 @@
 <?php
 //get the data from the form
-  $investment = filter_input(INPUT_POST, "investment");
-  $interest_rate = filter_input(INPUT_POST, "interest_rate");
-  $years = filter_input(INPUT_POST, "years");
+  $investment = filter_input(INPUT_POST, "investment", FILTER_VALIDATE_FLOAT);
+  $interest_rate = filter_input(INPUT_POST, "interest_rate", FILTER_VALIDATE_FLOAT);
+  $years = filter_input(INPUT_POST, "years", FILTER_VALIDATE_INT);
+
+//validate investment
+if($investment === FALSE) //IMPORTANT NOTES: "===" does not include the 0 in this error catch
+{
+  $error_message = "Investment must be a valid number.";  
+}
+else if ($investment <= 0)
+{
+  $error_message = "Investment must be greater than zero.";
+}
+
+else {
+  $error_message = "";
+}
 
 
+//if an error message exists, go to the index page
+if ($error_message != "")
+{
+  include("index.php");
+  exit();
+}
+
+//validate interest rate
+else if ($interest_rate === FALSE)
+{
+  $error_message = "Interest rate must be a valid number.";
+}
+else if ($interest_rate <= 0)
+{
+  $error_message = "Interest rate must be greater than zero";
+}
+else {
+  $error_message = "";
+}
+if ($error_message != "")
+{
+  include("index.php");
+  exit();
+}
 
 
+//validate years
+else if($years === FALSE)
+{
+  $error_message = "Years must be a valid whole number.";
+}
+
+else if ($years <= 0)
+{
+$error_message = "Years must be greater than zero.";
+}
+
+
+else if ($years > 30)
+{
+  $error_message = "Years must be less than 31.";
+}
+else
+{
+  $error_message = "";
+}
+
+
+if ($error_message != "")
+{
+  include("index.php");
+  exit();
+}
+
+
+//Calculate the future value
+$future_value = $investment;
+for($i = 1; $i <= $years; $i++)
+{
+  $future_value += $future_value * $interest_rate * 0.01;
+}
+
+
+//apply currency and percent formatting
+$investment_f = '$'.number_format($investment,2); //IMPORTANT: "." is the concatenator
+$yearly_rate_f = $interest_rate.'%';
+$future_value_f = '$'.number_format($future_value,2);
 
 
 ?>
@@ -25,13 +104,13 @@
        <h1>Future Value Calculator</h1>
 
        <label>Investment Amount:</label>
-       <span><?php echo $investment; ?></span><br>
+       <span><?php echo $investment_f; ?></span><br>
        <label>Yearly Interest Rate:</label>
-      <span><?php echo $interest_rate; ?></span><br>
+      <span><?php echo $yearly_rate_f; ?></span><br>
        <label>Number of Years:</label>
        <span><?php echo $years; ?></span><br>
        <label>Future Value:</label>
-       <span></span><br>
+       <span><?php echo $future_value_f; ?></span><br>
    </main> 
 
   </body>
